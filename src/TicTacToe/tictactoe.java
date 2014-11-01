113fe72c773ca31ca9c99c8f75b49e45046e0cad
Kari Mimisson <kari.mimisson@gmail.com>
2014-11-01 20:28:52 +0000

    Test to see if the grid is empty

--- src/TicTacToe/tictactoe.java
+++ src/TicTacToe/tictactoe.java
@@ -1,5 +1,17 @@
 package TicTacToe;
 
-public class tictactoe {
-
+public class tictactoe
+{
+	public char turn;
+	public char[] grid; 
+	public tictactoe()
+	{
+		this.grid = " ".toCharArray();
+		this.turn = 'X';
+	}
+	
+	public String toString()
+	{
+		return new String(grid);
+	}
 }
